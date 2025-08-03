3991ce7c01d8cc37b2ab4fa67136d56a33f2c29f
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2025-08-03 22:06:35 +0700

    feat: enable line indent for gitsigns

--- init.lua
+++ init.lua
@@ -5,20 +5,21 @@ vim.g.maplocalleader = "\\"
 vim.cmd("filetype plugin indent on")
 vim.cmd("syntax enable")
 vim.opt.number = true
+vim.o.signcolumn = "yes"
 
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- Tabbing behavior
-vim.opt.expandtab = true    -- Use spaces instead of tabs
-vim.opt.shiftwidth = 4      -- Size of an indent
-vim.opt.tabstop = 4         -- Number of spaces a tab counts for
-vim.opt.softtabstop = 4     -- Spaces when you press <Tab>
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4 -- Size of an indent
+vim.opt.tabstop = 4 -- Number of spaces a tab counts for
+vim.opt.softtabstop = 4 -- Spaces when you press <Tab>
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.hl.on_yank()
   end,
@@ -28,7 +29,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath})
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -46,7 +47,7 @@ require("lazy").setup({
   spec = {
     -- add your plugins here
     { import = "plugins" },
-    { import = "plugins.lsp" }
+    { import = "plugins.lsp" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
@@ -59,13 +60,13 @@ require("lazy").setup({
 vim.cmd("colorscheme tokyonight-night")
 
 -- Lualine
-require('lualine').setup({
-    options = {
-        theme = 'onedark'
-    }
+require("lualine").setup({
+  options = {
+    theme = "onedark",
+  },
 })
 
--- Import keymaps 
+-- Import keymaps
 require("keymaps")
 
 -- require("configs.lua_ls")

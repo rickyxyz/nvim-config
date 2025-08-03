73b9f6cc77b5e7792f760a4a14499be86c855897
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2025-08-03 22:03:18 +0700

    feat: add mason tool installer for auto install

---
+++ lua/plugins/lsp/mason-tool-installer.lua
@@ -0,0 +1,17 @@
+return {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = {
+        "mason-org/mason.nvim"
+    },
+    config = function()
+        local mason_tool_installer = require("mason-tool-installer")
+
+        mason_tool_installer.setup({
+            ensure_installed = {
+                "prettier",
+                "stylua",
+                "eslint_d"
+            }
+        })
+    end
+}
